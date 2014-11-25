b566dd400514226b4c79246d8937f0531df67707
unknown <t00173624@1XXB8CA3A80BED1.students.ittralee.local>
2014-11-25 10:46:03 +0000

    Code added to driver

--- QuarrySystemDriver.java
+++ QuarrySystemDriver.java
@@ -1,3 +1,23 @@
+import java.util.*;
+
 public class Driver{
 	
+	public static void main (String args[]){
+		
+		Scanner in = new Scanner(System.in);
+		
+		boolean quit = false;
+		int menuitem;
+		
+		do{
+			
+			System.out.print("Choose a menu item (Or enter 0 to exit):" );
+			menu Item = in.nextInt();
+			
+			switch
+			
+		}
+		
+	}
+	//http://www.javaforstudents.co.uk/Code_snippets/Switch-operated_text_menu
 }
